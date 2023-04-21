24c70288ff479e695002d4ea2304c200fc46ceb9
vikasredefinesolutions <88821386+vikasredefinesolutions@users.noreply.github.com>
2023-04-21 18:27:42 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -148,26 +148,20 @@ class MyDocument extends Document {
             dangerouslySetInnerHTML={{ __html: _globalStore.customScript }}
           ></script>
 
-        <script
-            type='text/javascript'
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WH227F7');`,
-            }}
-          ></script>
+       
         </Head>
         <body className='font-Outfit bg-white'>
-       
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WH227F7"
-        height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
-          
-          <Main />
-          <NextScript />
-
-          <script
+        <script
             dangerouslySetInnerHTML={{
               __html: _globalStore.customGlobalBodyScript,
             }}
           ></script>
+       
+          
+          <Main />
+          <NextScript />
+
+         
 
           <script
             dangerouslySetInnerHTML={{
