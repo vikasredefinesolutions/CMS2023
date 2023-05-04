ab8a04205e3b43d16850c0afb7891f6a9595184b
Lalit Barwal <Lalit@redefineindia.com>
2023-05-04 17:30:53 +0530

    punchout integration

---
+++ pages/punchout/index.tsx
@@ -0,0 +1,54 @@
+import { PunchoutPostApi } from '@services/punchout.service';
+import { DOMParser, XMLSerializer } from '@xmldom/xmldom';
+import fsPrmoises from 'fs/promises';
+import path from 'path';
+
+const Puchout = (props: any) => {
+  return <>{props}</>;
+};
+
+export default Puchout;
+
+export const getServerSideProps = async (context: any) => {
+  const req = context.req;
+  console.log(context, 'this is context');
+
+  const filePath = path.join(process.cwd(), '/public/success.xml');
+  const xmlData = await fsPrmoises.readFile(filePath);
+  let xmlDoc = xmlData.toLocaleString();
+
+  const parser = new DOMParser();
+  var doc = parser.parseFromString(xmlDoc, 'text/xml');
+  const serialized = new XMLSerializer().serializeToString(doc);
+
+  let body = '';
+  if (req.method == 'POST') {
+    req.on('data', (chunk: any) => {
+      body += chunk;
+    });
+    req.on('end', () => {});
+  }
+
+  const res = await PunchoutPostApi(serialized);
+  console.log(res, 'this si ');
+  return {
+    props: {
+      req: {
+        data: {
+          body: serialized,
+          headers: { ...context.req.headers },
+          returnUrl: { ...context.req?.return_url },
+        },
+      },
+      res: {
+        data: {
+          body: { ...context.res.body },
+          headers: { ...context.res.headers },
+          params: { ...context.req?.params },
+          returnUrl: { ...context.req?.return_url },
+        },
+      },
+      punchout: res,
+    },
+  };
+};
