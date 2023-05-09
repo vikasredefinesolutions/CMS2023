4a6e4f5a765bb63f90a40261e68dddbbca0f45f0
Lalit Barwal <Lalit@redefineindia.com>
2023-05-09 15:12:39 +0530

    testing punchout 10

--- pages/punchout/2go/start/index.tsx
+++ pages/punchout/2go/start/index.tsx
@@ -4,20 +4,24 @@ import getRawBody from 'raw-body';
 const Punchout = (props: any) => {
   const myHeaders = new Headers();
   myHeaders.append('Content-Type', 'application/xml');
+  myHeaders.append('Access-Control-Allow-Origin', ' no-cors');
+  console.log(props);
 
-  const requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: props.body,
-  };
+  if (props.body) {
+    const requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: props.body,
+    };
 
-  fetch(
-    'https://connect.punchout2go.com/gateway/link/return/id/ZH645a10448cef6',
-    requestOptions,
-  )
-    .then((response) => response.text())
-    .then((result) => console.log(result))
-    .catch((error) => console.log('error', error));
+    fetch(
+      'https://connect.punchout2go.com/gateway/link/return/id/ZH645a10448cef6',
+      requestOptions,
+    )
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log('error', error));
+  }
 
   return <>This page exists and getting response</>;
 };
