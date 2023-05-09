4acae69ec7fb533d4fbbd4bd93da932de279f0b7
Lalit Barwal <Lalit@redefineindia.com>
2023-05-09 16:01:10 +0530

    feat: added the xml file request to backend

--- pages/punchout/2go/start/index.tsx
+++ pages/punchout/2go/start/index.tsx
@@ -1,20 +1,48 @@
+import { PunchoutPostApi } from '@services/punchout.service';
 import getRawBody from 'raw-body';
 
-const Puchout = (props: any) => {
-  const params = new URLSearchParams(props.body);
-  let obj = {};
+const Punchout = (props: any) => {
+  const myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/xml');
+  myHeaders.append('Access-Control-Allow-Origin', ' no-cors');
+
+  if (props.body) {
+    const requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: props.body,
+    };
+
+    fetch(
+      'https://connect.punchout2go.com/gateway/link/return/id/ZH645a10448cef6',
+      requestOptions,
+    )
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log('error', error));
+  }
+
+  return <>This page exists and getting response</>;
+};
+
+export default Punchout;
+
+export const getServerSideProps = async (context: any) => {
+  const body = await getRawBody(context?.req);
+  const params = new URLSearchParams(body.toString());
+  let obj: Record<string, any> = {};
   obj = {
     pos: params.get('pos'),
     return_url: params.get('return_url'),
     params: JSON.parse(params.get('params') || ''),
   };
 
-  return <>This page exists and getting response</>;
-};
+  let a = `${JSON.stringify(obj)}`;
+  let b = '';
+  b = await PunchoutPostApi(a);
+  let returnxml = b
+    .toString()
+    .replace('###StoreUrl###', context.req.headers.host);
 
-export default Puchout;
-
-export const getServerSideProps = async (context: any) => {
-  const body = await getRawBody(context?.req);
-  return { props: { body: body.toString() } };
+  return { props: { body: returnxml, returnUrl: obj.return_url } };
 };
