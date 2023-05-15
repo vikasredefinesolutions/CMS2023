a5acbddd3ddad321a8e3020b1301c62f0c37b625
= <=>
2023-05-15 16:56:46 +0530

    Mens category Issue

--- next.config.js
+++ next.config.js
@@ -4,14 +4,14 @@ const nextConfig = {
   productionBrowserSourceMaps: true,
   images: {
     domains: [
-      "redefinecommerce.blob.core.windows.net",
-      "redefinecommerce.blob.core.windows.netstring",
+      'redefinecommerce.blob.core.windows.net',
+      'redefinecommerce.blob.core.windows.netstring',
     ],
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "redefinecommerce.blob.core.windows.net",
-        pathname: "**",
+        protocol: 'https',
+        hostname: 'redefinecommerce.blob.core.windows.net',
+        pathname: '**',
       },
       // {
       //   protocol: 'https',
@@ -19,6 +19,12 @@ const nextConfig = {
       //   pathname: '**',
       // },
     ],
+  },
+  experimental: {
+    swcMinify: true,
+    // set large page allocation size to 1GB
+    workerThreads: true,
+    largePageDataBytes: 1024 * 1024 * 1024,
   },
 };
 
