36e1fbbda47c05b1f4cc5a5b05e7406eecadf04d
= <=>
2023-05-16 10:10:28 +0530

    ESLint iisue

--- next.config.js
+++ next.config.js
@@ -20,15 +20,7 @@ const nextConfig = {
       // },
     ],
   },
-  rules: {
-    '@typescript-eslint/no-non-null-assertion': 0,
-  },
-  experimental: {
-    swcMinify: true,
-    // set large page allocation size to 1GB
-    workerThreads: true,
-    largePageDataBytes: 1024 * 1024 * 1024,
-  },
+ 
 };
 
 module.exports = nextConfig;
