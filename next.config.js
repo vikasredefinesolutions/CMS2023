c81726820eb88cc357542af73d486b4bc00ef0cf
= <=>
2023-05-23 22:15:41 +0530

    Initial Loading and Script Changes

--- next.config.js
+++ next.config.js
@@ -12,12 +12,12 @@ const nextConfig = {
       },
     ],
   },
-  experimental: {
-    swcMinify: true,
-    // set large page allocation size to 1GB
-    workerThreads: true,
-    largePageDataBytes: 1024 * 1024 * 1024,
-  },
+  // experimental: {
+  //   swcMinify: true,
+  //   // set large page allocation size to 1GB
+  //   workerThreads: true,
+  //   largePageDataBytes: 1024 * 1024 * 1024,
+  // },
 };
 
 module.exports = nextConfig;
