dfccfe71936988d9231e46c62cc471ebbae701da
= <=>
2023-05-26 22:36:12 +0530

    Wishlist

--- Templates/Header/header_Type5/Components/Icons/Header_WishListIcon.tsx
+++ Templates/Header/header_Type5/Components/Icons/Header_WishListIcon.tsx
@@ -35,7 +35,7 @@ const WishListIcon: React.FC = () => {
         >
           <span className='sr-only'>Wishlist</span>{' '}
           <span className='material-icons-outlined'>favorite_border</span>{' '}
-          <span className='absolute right-[-7px] top-[-1px] rounded-full flex items-center justify-center bg-[#dddddd] text-[9px] text-[#000000] pl-[4px] pr-[4px] pt-[2px] pb-[2px]'>
+          <span className='absolute right-[-7px] top-[-1px] rounded-full flex items-center justify-center bg-[#dddddd] text-[9px] text-[#000000] pl-[4px] pr-[4px] pt-[2px] pb-[2px] leading-[12px]'>
             {wishlist ? wishlist.length : 0}
           </span>
         </a>
