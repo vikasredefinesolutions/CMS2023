29dd04e8f16f70ca202793971ffc3f89aee3e522
Vikas R. Patel <vikas@kaushalaminc.com>
2023-06-06 21:13:12 +0530

    latest code

--- pages/catalogs/index.tsx
+++
@@ -1,9 +0,0 @@
-import { _defaultTemplates } from '@configs/template.config';
-import CatalogDisplayBrands from '@templates/Catalogs';
-import { NextPage } from 'next';
-
-const ShowCatalogs: NextPage = () => {
-  return <CatalogDisplayBrands id={_defaultTemplates.catalogs} />;
-};
-
-export default ShowCatalogs;
--- pages/clearCache/index.tsx
+++
@@ -1,9 +0,0 @@
-const ClearCache = () => {
-  return (
-    <>
-      <h1>Clear Cache</h1>
-    </>
-  );
-};
-
-export default ClearCache;
