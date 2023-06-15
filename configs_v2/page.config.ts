f4f9a34f7be75f3350cd417ba5052805d3351c99
Lalit Barwal <Lalit@redefineindia.com>
2023-06-15 15:01:03 +0530

    live site and payload changes

--- configs_v2/page.config.ts
+++ configs_v2/page.config.ts
@@ -50,8 +50,8 @@ export const _StoreDomains = {
 //////////////////////////////////////////////////////////////////////
 
 export const __domain = {
-  isSiteLive: false,
-  localDomain: _StoreDomains.domain5,
+  isSiteLive: true,
+  localDomain: _StoreDomains.domain22,
 };
 
 export const defaultCountry = 1;
