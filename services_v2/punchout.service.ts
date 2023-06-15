c562367b4b325b52683542b51e50e9ce34c58890
Lalit Barwal <Lalit@redefineindia.com>
2023-06-15 15:16:29 +0530

    testing 13

--- services_v2/punchout.service.ts
+++ services_v2/punchout.service.ts
@@ -2,11 +2,12 @@ import { SendAsync } from '@utils/axios.util';
 import { AxiosRequestConfig } from 'axios';
 
 export const PunchoutPostApi = async (body: string) => {
+  console.log(JSON.parse(body), 'JS....................');
   const url = '/Punchout/index.json';
   const res: any = await SendAsync<AxiosRequestConfig>({
     url: url,
     method: 'POST',
-    data: `${body}`,
+    data: body,
   });
   return res;
 };
