a8794cfc5e1b9ee869a91de0f66e99474b45d13d
lalitbarwalredefine <lalit@redefinecommerce.com>
2023-08-09 12:04:08 +0530

    testing 2

--- pages/punchout/2go/start/index.tsx
+++ pages/punchout/2go/start/index.tsx
@@ -20,6 +20,7 @@ const Punchout = (props: any) => {
         let str = `${res}`;
         let str1 = str.split('<URL>');
         let str2 = str1[1].split('</URL>');
+        console.log(str2[0]);
         window.open(str2[0], '_blank');
       })
       .catch((err) => console.log(err));
