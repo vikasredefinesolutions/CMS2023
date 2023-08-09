8f72fade6083e242fdc687e9f1545cd68c435e3c
lalitbarwalredefine <lalit@redefinecommerce.com>
2023-08-09 10:40:30 +0530

    punchout testing

--- pages/punchout/2go/start/index.tsx
+++ pages/punchout/2go/start/index.tsx
@@ -1,22 +1,37 @@
+import { PunchoutPostApi } from '@services/punchout.service';
 import getRawBody from 'raw-body';
+import { useEffect } from 'react';
 
-const Puchout = (props: any) => {
-  const params = new URLSearchParams(props.body);
-  let obj = {};
-  obj = {
-    pos: params.get('pos'),
-    return_url: params.get('return_url'),
-    params: JSON.parse(params.get('params') || ''),
-  };
+const Punchout = (props: any) => {
+  useEffect(() => {
+    const params = new URLSearchParams(props.body);
+    let obj: Record<string, any> = {};
+    obj = {
+      pos: params.get('pos'),
+      return_url: params.get('return_url'),
+      params: JSON.parse(params.get('params') || ''),
+    };
+    let a = `${JSON.stringify(obj)}`;
+    let b = '';
 
-  console.log(obj, 'the object is');
+    PunchoutPostApi(a)
+      .then((res) => {
+        let str = `${res}`;
+        let str1 = str.split('<URL>');
+        let str2 = str1[1].split('</URL>');
+        window.open(str2[0], '_blank');
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return <>This page exists and getting response</>;
 };
 
-export default Puchout;
+export default Punchout;
 
 export const getServerSideProps = async (context: any) => {
   const body = await getRawBody(context?.req);
-  return { props: { body: body.toString() } };
+  return {
+    props: { body: body.toString(), returnUrl: context.req.headers.host },
+  };
 };
