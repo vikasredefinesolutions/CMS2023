2041f8cb1a8bac094b5c320ecb5a6a608a9a4fef
lalitbarwalredefine <lalit@redefinecommerce.com>
2023-08-10 12:12:18 +0530

    punchout testing 6

--- pages/punchout/2go/start/index.tsx
+++ pages/punchout/2go/start/index.tsx
@@ -1,50 +1,48 @@
 import { PunchoutPostApi } from '@services/punchout.service';
+import axios from 'axios';
 import getRawBody from 'raw-body';
-import { useEffect, useState } from 'react';
 
 const Punchout = (props: any) => {
-  const [returnXml, setReturnXml] = useState<any>('');
+  // const [returnXml, setReturnXml] = useState<any>('');
   console.log(props.returnUrl);
   // let returnxml = '';
 
-  useEffect(() => {
-    const params = new URLSearchParams(props.body);
-    let obj: Record<string, any> = {};
-    obj = {
-      pos: params.get('pos'),
-      return_url: params.get('return_url'),
-      params: JSON.parse(params.get('params') || ''),
-    };
-    let a = `${JSON.stringify(obj)}`;
-    let b = '';
-    const fetchData = async (a: any) => {
-      b = await PunchoutPostApi(a);
-      console.log(b, 'coming from backend'),
-        setReturnXml(
-          b.toString().replace('###StoreUrl###', `https://${props.returnUrl}`),
-        );
-    };
-    fetchData(a);
-    //   let config = {
-    //     method: 'post',
-    //     maxBodyLength: Infinity,
-    //     url: obj.return_url,
-    //     withCredentials: false,
-    //     headers: {
-    //       'Content-Type': 'application/xml',
-    //       'Access-Control-Allow-Origin': '*',
-    //     },
-    //     data: returnXml,
-    //   };
-    //   axios
-    //     .request(config)
-    //     .then((response) => {
-    //       console.log(JSON.stringify(response.data));
-    //     })
-    //     .catch((err) => console.log(err));
-  }, []);
-
-  console.log(returnXml, 'xmllllll');
+  // useEffect(() => {
+  //   const params = new URLSearchParams(props.body);
+  //   let obj: Record<string, any> = {};
+  //   obj = {
+  //     pos: params.get('pos'),
+  //     return_url: params.get('return_url'),
+  //     params: JSON.parse(params.get('params') || ''),
+  //   };
+  //   let a = `${JSON.stringify(obj)}`;
+  //   let b = '';
+  //   const fetchData = async (a: any) => {
+  //     b = await PunchoutPostApi(a);
+  //     console.log(b, 'coming from backend'),
+  //       setReturnXml(
+  //         b.toString().replace('###StoreUrl###', `https://${props.returnUrl}`),
+  //       );
+  //   };
+  //   fetchData(a);
+  //   let config = {
+  //     method: 'post',
+  //     maxBodyLength: Infinity,
+  //     url: obj.return_url,
+  //     withCredentials: false,
+  //     headers: {
+  //       'Content-Type': 'application/xml',
+  //       'Access-Control-Allow-Origin': '*',
+  //     },
+  //     data: returnXml,
+  //   };
+  //   axios
+  //     .request(config)
+  //     .then((response) => {
+  //       console.log(JSON.stringify(response.data));
+  //     })
+  //     .catch((err) => console.log(err));
+  // }, []);
 
   // const myHeaders = new Headers();
   // myHeaders.append('Content-Type', 'application/xml');
@@ -73,6 +71,36 @@ export default Punchout;
 
 export const getServerSideProps = async (context: any) => {
   const body = await getRawBody(context?.req);
+  const params = new URLSearchParams(body.toString());
+  let obj: Record<string, any> = {};
+  obj = {
+    pos: params.get('pos'),
+    return_url: params.get('return_url'),
+    params: JSON.parse(params.get('params') || ''),
+  };
+  let a = `${JSON.stringify(obj)}`;
+  let b = '';
+  const fetchData = async (a: any) => {
+    b = await PunchoutPostApi(a);
+  };
+  fetchData(a);
+  let config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: obj.return_url,
+    withCredentials: false,
+    headers: {
+      'Content-Type': 'application/xml',
+      'Access-Control-Allow-Origin': '*',
+    },
+    data: b,
+  };
+  axios
+    .request(config)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch((err) => console.log(err));
   return {
     props: { body: body.toString(), returnUrl: context.req.headers.host },
   };
