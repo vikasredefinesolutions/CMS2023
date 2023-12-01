40ec5c31739999be5ef7fc5194c0c94833edfc0c
vikasredefinesolutions <88821386+vikasredefinesolutions@users.noreply.github.com>
2023-12-01 15:32:13 +0530

    Update SearchProduct.tsx

--- appComponents_v2/reUsable/SearchProduct.tsx
+++ appComponents_v2/reUsable/SearchProduct.tsx
@@ -1,9 +1,11 @@
 import NxtImage from '@appComponents/reUsable/Image';
 import Price from '@appComponents/reUsable/Price';
 import {
+  BACARDI,
   BOSTONBEAR,
   HEALTHYPOINTS,
   SIMPLI_SAFE_CODE,
+  THD_STORE_CODE,
   UCA,
   UNITI_CODE,
   _Store_CODES,
@@ -30,8 +32,12 @@ const SearchProductCard: React.FC<props> = ({ product }) => {
       <div
         key={product.id}
         className={`relative ${
-          storeCode === _Store_CODES.USAAHEALTHYPOINTS
+          storeCode === _Store_CODES.USAAHEALTHYPOINTS ||
+          storeCode === THD_STORE_CODE ||
+          storeCode === _Store_CODES.USAAALPHAHEALTHYPOINTS
             ? 'border border-transparent hover:border-gray-border hover:shadow-md'
+            : storeCode == BACARDI
+            ? ''
             : 'border border-gray-200'
         }  p-[20px]`}
       >
@@ -44,9 +50,9 @@ const SearchProductCard: React.FC<props> = ({ product }) => {
           >
             <a className='relative'>
               <NxtImage
-                title={product.name}
-                src={mainImageUrl.attributeImage || null}
-                alt={product.name}
+                title={product?.name}
+                src={mainImageUrl?.attributeImage || null}
+                alt={product?.name}
                 className='m-auto max-h-full'
               />
             </a>
@@ -54,13 +60,18 @@ const SearchProductCard: React.FC<props> = ({ product }) => {
         </div>
         <div
           className={`${
-            storeCode !== _Store_CODES.USAAHEALTHYPOINTS && 'mt-[24px]'
+            storeCode !== _Store_CODES.USAAHEALTHYPOINTS &&
+            storeCode !== _Store_CODES.USAAALPHAHEALTHYPOINTS &&
+            'mt-[24px]'
           } pl-[8px] pr-[8px]`}
         >
           <div
             className={`  text-anchor hover:text-anchor-hover text-ellipsis overflow-hidden line-clamp-2 ${
-              storeCode === _Store_CODES.USAAHEALTHYPOINTS
+              storeCode === _Store_CODES.USAAHEALTHYPOINTS ||
+              storeCode === _Store_CODES.USAAALPHAHEALTHYPOINTS
                 ? 'text-extra-small-text mt-[10px]'
+                : storeCode === THD_STORE_CODE
+                ? 'text-default-text mt-[14px] h-[44px]'
                 : 'text-medium-text mt-[14px] h-[44px]'
             }`}
           >
@@ -68,38 +79,70 @@ const SearchProductCard: React.FC<props> = ({ product }) => {
               key={product.id}
               href={`/${encodeURIComponent(product.seName)}.html`}
             >
-              <a className='relative text-anchor hover:text-anchor '>
+              <a
+                className={` relative text-anchor hover:text-anchor ${
+                  storeCode === _Store_CODES.USAAPUNCHOUT ? 'font-semibold' : ''
+                }`}
+              >
                 {product.name}
               </a>
             </Link>
           </div>
 
           <div
-            className={` text-[#000000] ${
-              storeCode === _Store_CODES.USAAHEALTHYPOINTS
-                ? 'text-sub-text mt-[5px]'
-                : 'text-normal-text mt-[12px]'
-            } text-medium-text tracking-wider`}
+            className={
+              storeCode === _Store_CODES.USAAPUNCHOUT
+                ? ' mb-[12px] text-sub-text'
+                : ` text-[#000000] ${
+                    storeCode === _Store_CODES.USAAHEALTHYPOINTS ||
+                    storeCode === _Store_CODES.USAAALPHAHEALTHYPOINTS
+                      ? 'text-sub-text mt-[5px]'
+                      : 'text-normal-text mt-[12px]'
+                  } text-medium-text tracking-wider ${
+                    storeCode === THD_STORE_CODE
+                      ? 'flex flex-wrap items-center justify-center gap-x-1'
+                      : ''
+                  } `
+            }
           >
             <span
               className={`${
-                storeCode === _Store_CODES.USAAHEALTHYPOINTS
+                storeCode === _Store_CODES.USAAHEALTHYPOINTS ||
+                storeCode === _Store_CODES.USAAALPHAHEALTHYPOINTS ||
+                storeCode === _Store_CODES.USAAPUNCHOUT
                   ? 'text-primary'
+                  : storeCode === THD_STORE_CODE
+                  ? 'flex items-center'
                   : 'font-semibold'
               }`}
             >
               {storeCode == SIMPLI_SAFE_CODE ||
               storeCode == UCA ||
-              storeCode == HEALTHYPOINTS
+              storeCode == HEALTHYPOINTS ||
+              storeCode == _Store_CODES.USAAALPHAHEALTHYPOINTS ||
+              storeCode == BACARDI ||
+              storeCode == BOSTONBEAR ||
+              storeCode == _Store_CODES.USAAPUNCHOUT
                 ? ''
-                : 'MSRP'}
-              <Price value={product.msrp} />
+                : storeCode === THD_STORE_CODE
+                ? 'STARTING '
+                : 'MSRP '}
+              <span
+                className={`font-semibold ${
+                  storeCode === BACARDI
+                    ? 'text-sub-text text-secondary !font-bold'
+                    : 'text-primary text-sub-text'
+                } ${storeCode === THD_STORE_CODE ? ' ml-[10px]' : ''}`}
+              >
+                <Price value={product.msrp} />
+              </span>
             </span>
           </div>
 
           <ul
             className={`flex items-center ${
-              storeCode === _Store_CODES.USAAHEALTHYPOINTS
+              storeCode === _Store_CODES.USAAHEALTHYPOINTS ||
+              storeCode === _Store_CODES.USAAALPHAHEALTHYPOINTS
                 ? ' my-[4px]'
                 : ' my-[10px]'
             } justify-center space-x-1`}
@@ -107,13 +150,35 @@ const SearchProductCard: React.FC<props> = ({ product }) => {
             {product.getAttributeImageLists.map((res: any) => {
               return (
                 <li
-                  className={`w-[30px] h-[30px] p-[1px] border-2  hover:border-secondary cursor-pointer ${
+                  className={`w-[30px] h-[30px] p-[1px] ${
+                    storeCode == BACARDI || storeCode == BOSTONBEAR
+                      ? 'border'
+                      : 'border-2'
+                  } ${
+                    storeCode !== THD_STORE_CODE
+                      ? storeCode == BOSTONBEAR
+                        ? ' hover:border-quaternary'
+                        : 'hover:border-secondary'
+                      : ''
+                  } 
+                  
+                  cursor-pointer ${
                     mainImageUrl.attributeOptionID == res.attributeOptionID
-                      ? 'border-secondary'
+                      ? storeCode == BACARDI
+                        ? 'border-default'
+                        : storeCode == BOSTONBEAR
+                        ? ' border-quaternary'
+                        : 'border-secondary'
                       : ''
                   } `}
                   onClick={() => {
                     setMainImageUrl(res);
+                  }}
+                  onMouseEnter={() => {
+                    setMainImageUrl(res);
+                  }}
+                  onMouseLeave={() => {
+                    setMainImageUrl(product.getAttributeImageLists[0]);
                   }}
                 >
                   <NxtImage
